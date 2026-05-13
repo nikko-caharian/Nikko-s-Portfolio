1c25ed8cb7b712f3169a1f2e48212e9711e7f493
Cesar Nikko Caharian III <nikko@caharian.com>
2026-05-13 23:38:35 +0800

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import { useEffect } from "react";
 import Layout from "./components/Layout";
 import Home from "./pages/Home";
 import About from "./pages/About";
+import Portfolio from "./pages/Portfolio";
 
 function ScrollToTop() {
   const { pathname } = useLocation();
@@ -25,6 +26,7 @@ export default function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
+          <Route path="/portfolio" element={<Portfolio />} />
         </Routes>
       </Layout>
     </BrowserRouter>
